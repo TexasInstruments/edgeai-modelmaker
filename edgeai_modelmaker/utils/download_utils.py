25a3cfc4fb395be9fb694cc03decb0457e4cbd43
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-08 19:03:36 +0000

    Adding tarfile member sanitization to extractall()

--- edgeai_modelmaker/utils/download_utils.py
+++ edgeai_modelmaker/utils/download_utils.py
@@ -55,12 +55,50 @@ def extract_files(download_file, extract_root):
     #
     if download_file.endswith('.tar'):
         with tarfile.open(download_file, 'r') as tar:
-            tar.extractall(path=extract_root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar, path=extract_root)
         #
         extract_success = True
     elif download_file.endswith('.tar.gz') or download_file.endswith('.tgz'):
         with tarfile.open(download_file, 'r:gz') as tar:
-            tar.extractall(path=extract_root)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(tar, path=extract_root)
         #
         extract_success = True
     elif download_file.endswith('.gz'):
